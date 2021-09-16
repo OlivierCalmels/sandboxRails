7e3916865734f4969ffd9d920d6adba0114e9024
olivier calmels <calmels.o@gmail.com>
2021-09-16 11:51:23 +0200

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,13 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Dog.delete_all
+
 Dog.create(name: 'Nala', motto: 'Born to be wild')
 Dog.create(name: 'Alex', motto: 'Calm as can be')
 Dog.create(name: 'Leroy', motto: 'Life of the pawty')
 Dog.create(name: 'Belle', motto: 'Miss Independent')
+Dog.create(name: 'Caroline', motto: "Plus ultra")
+Dog.create(name: 'Dania', motto: "À cœur vaillant, rien d'impossible")
+Dog.create(name: 'Matthieu', motto: "Qui s'y frotte s'y pique")
+Dog.create(name: 'Olivier', motto: 'Vive le code!')
